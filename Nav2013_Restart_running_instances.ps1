7ef83895b5e71964cfef64508342339710228752
theschitz <theodore@theschitz.se>
2016-08-23 14:15:22 +0200

    New Script
    First.

---
+++ Nav2013_Restart_running_instances.ps1
@@ -0,0 +1,11 @@
+﻿<# Load CMDlets #>
+#Import-Module 'C:\Program Files (x86)\Microsoft Dynamics NAV\70\RoleTailored Client\Microsoft.Dynamics.Nav.Model.Tools.psd1' -WarningAction SilentlyContinue | out-null
+Import-Module 'C:\Program Files\Microsoft Dynamics NAV\70\Service\NavAdminTool.ps1' -WarningAction SilentlyContinue | Out-Null
+
+$Instances = Get-NAVServerInstance
+
+for ($i = 0; $i -le $Instances.Count - 1; $i++) {    
+    if ($Instances[$i].State -eq 'Running') {        
+		Restart-Service $Instances[$i].Name
+    }
+}
