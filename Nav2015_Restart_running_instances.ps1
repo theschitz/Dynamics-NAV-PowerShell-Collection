dbe1d6378162d9275fbd26b8e60b826e2f7fb20d
theschitz <theodore@theschitz.se>
2016-08-24 13:36:49 +0200

    new script
    Script for 2015 and up.

---
+++ Nav2015_Restart_running_instances.ps1
@@ -0,0 +1,12 @@
+﻿<# Load CMDlets #>
+Import-Module 'C:\Program Files (x86)\Microsoft Dynamics NAV\80\RoleTailored Client\Microsoft.Dynamics.Nav.Model.Tools.psd1' -WarningAction SilentlyContinue | out-null
+Import-Module 'C:\Program Files\Microsoft Dynamics NAV\80\Service\NavAdminTool.ps1' -WarningAction SilentlyContinue | Out-Null
+
+$Instances = Get-NAVServerInstance
+
+for ($i = 0; $i -le $Instances.Count - 1; $i++) {    
+    if ($Instances[$i].State -eq 'Running') {        
+		 $Instances[$i].ServerInstance
+         Set-NAVServerInstance $Instances[$i].ServerInstance -Restart    
+    }
+}
