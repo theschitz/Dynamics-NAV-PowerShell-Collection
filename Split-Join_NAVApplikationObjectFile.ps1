5c40ca2685f7e5df86c4547f685e4f5d4c38db09
theschitz <theodore@theschitz.se>
2016-08-31 09:52:49 +0200

    New: Split / Join script

---
+++ Split-Join_NAVApplikationObjectFile.ps1
@@ -0,0 +1,12 @@
+﻿# Load CMDlets
+Import-Module 'C:\Program Files (x86)\Microsoft Dynamics NAV\90\RoleTailored Client\Microsoft.Dynamics.Nav.Model.Tools.psd1' -WarningAction SilentlyContinue | out-null
+Import-Module 'C:\Program Files\Microsoft Dynamics NAV\90\Service\NavAdminTool.ps1' -WarningAction SilentlyContinue | Out-Null
+
+$SplitSource  = 'C:\temp\split.txt'
+$SplitDestination = 'C:\temp\objects\'
+#$JoinSource = 'C:\temp\objects\'
+#$JoinDestination = 'C:\temp\join.txt'
+
+Split-NAVApplicationObjectFile -Source $SplitSource -Destination $SplitDestination
+
+#Join-NAVApplicationObjectFile -Source $JoinSource -Destination $JoinDestination
